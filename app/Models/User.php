ca153f40cdfebce976604aea49017e2d4e63c3b2
Peter Lindley <peter@judgeservice.com>
2021-03-31 12:01:50 +0100

    Added source files and complied them using webpack.

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
+        'username',
+        'avatar',
         'name',
         'email',
         'password',
+        'is_active'
     ];
 
     /**
@@ -40,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles() {
+        return $this->belongsToMany(Role::class);
+    }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +11,30 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+mix.js(
+    [
+        "resources/js/app.js",
+        "resources/js/bootstrap.js",
+        "resources/js/bootstrap.min.js",
+        "resources/js/jquery.js",
+        "resources/js/metisMenu.js",
+        "resources/js/sb-admin-2.js",
+        "resources/js/scripts.js",
+    ],
+    "public/js/libs.js"
+)
+    .styles(
+        [
+            "resources/css/app.css",
+            "resources/css/blog-post.css",
+            "resources/css/bootstrap.css",
+            "resources/css/bootstrap.min.css",
+            "resources/css/font-awesome.css",
+            "resources/css/metisMenu.css",
+            "resources/css/sb-admin-2.css",
+            "resources/css/styles.css",
+        ],
+        "public/css/libs.css"
+    )
+    .sass("resources/sass/app.scss", "public/css")
     .sourceMaps();
